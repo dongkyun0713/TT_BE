41db5c3cbde4726e8f5aacfb8504188d468d048f
dd-jiyun <dlwldbs8015@gmail.com>
2024-01-11 00:22:27 +0900

    Initial settings

---
+++ src/main/java/skhu/skhu_titto/SkhuTittoApplication.java
@@ -0,0 +1,13 @@
+package skhu.skhu_titto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuTittoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SkhuTittoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/skhu/skhu_titto/SkhuTittoApplicationTests.java
@@ -0,0 +1,13 @@
+package skhu.skhu_titto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuTittoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
